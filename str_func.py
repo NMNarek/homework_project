4a5e018aa0926f430a0f1198d250d44b94f4ca04
NMNarek <narek8222@gmail.com>
2024-05-12 11:43:03 +0200

    add def capitalized_string add docstring for def user_word

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def user_word(word: str):
+    """
+
+    :param word:
+    :return: Слово заглавными буквами
+    """
     return word.upper()
+
+
+def capitalized_string(user_str: str):
+    """
+
+    :param user_str:
+    :return: Строка с первыми заглавными буквами
+    """
+    return user_str.capitalize()
